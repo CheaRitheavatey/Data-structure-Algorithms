ee58d5361712c705730669922dbdade28fb77ba6
CheaRitheavatey <2023447chea@aupp.edu.kh>
2025-01-14 14:28:53 +0200

    Update LeetcodePractice.java

--- LeetcodePractice.java
+++ LeetcodePractice.java
@@ -1,10 +1,32 @@
+import java.util.Arrays;
+
 public class LeetcodePractice {
+    
+        public static int hIndex(int[] citations) {
+            // sort array
+            Arrays.sort(citations); // time complexity O(nlogn)
+            System.out.println(Arrays.toString(citations));
+
+            int result = 0;
+            for (int i=0; i<citations.length;i++) {
+                if (citations[i] >= citations.length -i) {
+                    result = citations.length - i;
+                    break;
+                }
+            } // time complexity O(n)
+            return result;
+
+            // time complexity O(nlogn) + O(n) = O(nlogn)
+        }
+    
+        
+    
     public static String longestCommonPrefix(String[] strs) {
         String output = "";
         for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
             for (int j =1; j < strs.length; j++) {
-                if (strs[j].charAt(i) != c) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
                     return output;
                 }
             }
@@ -16,7 +38,9 @@ public class LeetcodePractice {
 
     public static void main(String[] args) {
         String[] strs = {"flower","flow","flight"};
-        System.out.println(longestCommonPrefix(strs));
+        // System.out.println(longestCommonPrefix(strs));
+        int[] citations = {1,3,1};
+        System.out.println(hIndex(citations));
 
     }
 }
